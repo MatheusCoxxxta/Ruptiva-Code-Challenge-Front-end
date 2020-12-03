ee062c1ed0561bad88b5e6262438ab0a790ca171
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-03 15:11:56 -0300

    Creating mocked array of users

---
+++ utils/usersMock.ts
@@ -0,0 +1,54 @@
+let users: { id: number; name: string; document: string }[] = [
+  {
+    id: 1,
+    name: "Matheus",
+    document: "49818286839",
+  },
+  {
+    id: 2,
+    name: "Matheus",
+    document: "49818286839",
+  },
+  {
+    id: 3,
+    name: "Matheus",
+    document: "49818286839",
+  },
+  {
+    id: 4,
+    name: "Matheus",
+    document: "49818286839",
+  },
+  {
+    id: 5,
+    name: "Matheus",
+    document: "49818286839",
+  },
+  {
+    id: 6,
+    name: "Matheus",
+    document: "49818286839",
+  },
+  {
+    id: 7,
+    name: "Matheus",
+    document: "49818286839",
+  },
+  {
+    id: 8,
+    name: "Matheus",
+    document: "49818286839",
+  },
+  {
+    id: 9,
+    name: "Matheus",
+    document: "49818286839",
+  },
+  {
+    id: 10,
+    name: "Matheus",
+    document: "49818286839",
+  },
+];
+
+export default users;
