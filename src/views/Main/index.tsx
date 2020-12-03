cb0a392999e5d23158995588ffbcdaa7bb50eb67
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-03 13:43:48 -0300

    Creating components and organizing files structure

---
+++ src/views/Main/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Form from "../../components/Main/Form/"
+import UserList from "../../components/Main/UserList"
+
+const Main = () => {
+  return (
+    <>
+      <Form />
+      <UserList />
+    </>
+  );
+}
+
+export default Main
