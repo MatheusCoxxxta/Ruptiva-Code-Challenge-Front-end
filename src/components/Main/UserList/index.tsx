394e5214c3aed5b892946c05131dd39db9f73968
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-05 02:00:35 -0300

    New style to user list component

--- src/components/Main/UserList/index.tsx
+++ src/components/Main/UserList/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
+import { StyleSheet, Image, TouchableOpacity, Alert, View } from "react-native";
 import { DataTable } from "react-native-paper";
 import styled from "styled-components/native";
 import { cpfMask, cnpjMask } from "../../../../tools/documentMasks";
 import { Feather } from "@expo/vector-icons";
 import { User } from "../../../store/users/types";
+
+import {
+  Title,
+  Box,
+  ScrollBox,
+  Row,
+  UserBox,
+  ImageBg,
+  TextCol,
+  RowName,
+  RowDoc,
+} from "./style";
 
 const UserList = (props: {
   users: { _id?: string; name: string; document: string; type: string }[];
@@ -24,16 +30,9 @@ const UserList = (props: {
 
   return (
     <>
-      <Title>Usuários</Title>
-      <DataTable style={[styles.dataTable, styles.dataTableHeader]}>
-        <DataTable.Header>
-          <DataTable.Title> </DataTable.Title>
-          <DataTable.Title>Nome</DataTable.Title>
-          <DataTable.Title>Documento do Cliente</DataTable.Title>
-        </DataTable.Header>
-      </DataTable>
-      <ScrollView style={styles.container}>
-        <DataTable style={styles.dataTable}>
+      <Box style={styles.container}>
+        <Title>Usuários</Title>
+        <ScrollBox>
           {users.map(
             (user: {
               _id?: string;
@@ -41,61 +40,56 @@ const UserList = (props: {
               document: string;
               type: string;
             }) => (
-              <DataTable.Row key={user._id}>
-                <DataTable.Cell>
-                  <TouchableOpacity
-                    onPress={() =>
-                      Alert.alert(
-                        "Atenção!",
-                        "Gostaria de deletar o usuário?",
-                        [
-                          {
-                            text: "Cancelar",
-                            onPress: () => Alert.alert("Ação cancelada."),
-                            style: "cancel",
-                          },
-                          {
-                            text: "Sim",
-                            onPress: () => handleDelete(user),
-                          },
-                        ]
-                      )
-                    }
-                  >
-                    <Feather name="trash" size={24} color="black" />
-                  </TouchableOpacity>
-                </DataTable.Cell>
-                <DataTable.Cell>
-                  <Text
-                    style={
-                      user.type === "individual"
-                        ? { color: "#ff6a00" }
-                        : { color: "#00b2ff" }
-                    }
-                  >
-                    {user.name}
-                  </Text>
-                </DataTable.Cell>
-
-                <DataTable.Cell>
-                  {user.document.length === 11
-                    ? cpfMask(user.document)
-                    : cnpjMask(user.document)}
-                </DataTable.Cell>
-              </DataTable.Row>
+              <Row key={user._id} style={{ flex: 1 }}>
+                <UserBox>
+                  <ImageBg>
+                    <Image
+                      style={{ width: 30, height: 30 }}
+                      source={
+                        user.type === "business"
+                          ? require("../../../../assets/Business.png")
+                          : require("../../../../assets/Individual.png")
+                      }
+                    />
+                  </ImageBg>
+                  <TextCol>
+                    <RowName>{user.name}</RowName>
+                    <RowDoc
+                      style={
+                        user.type === "individual"
+                          ? { color: "#ff6a00" }
+                          : { color: "#00b2ff" }
+                      }
+                    >
+                      {user.document}
+                    </RowDoc>
+                  </TextCol>
+                </UserBox>
+                <TouchableOpacity
+                  onPress={() =>
+                    Alert.alert("Atenção!", "Gostaria de deletar o usuário?", [
+                      {
+                        text: "Cancelar",
+                        onPress: () => Alert.alert("Ação cancelada."),
+                        style: "cancel",
+                      },
+                      {
+                        text: "Sim",
+                        onPress: () => handleDelete(user),
+                      },
+                    ])
+                  }
+                >
+                  <Feather name="trash" size={24} color="white" />
+                </TouchableOpacity>
+              </Row>
             )
           )}
-        </DataTable>
-      </ScrollView>
+        </ScrollBox>
+      </Box>
     </>
   );
 };
-
-const Title = styled.Text`
-  margin-bottom: 10px;
-  font-size: 15px;
-  font-weight: bold;
-`;
 
 const styles = StyleSheet.create({
   container: {
